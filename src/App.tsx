a81fc3e409f56f86597ee932b5a06453fb2907ec
SethminiDissanayake <sethminidissa@gmail.com>
2024-05-25 11:12:13 +0530

    create Footer.tsx file

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,14 @@ import logo from './logo.svg';
 import {NavBar} from './view/Navbar/NavBar';
 import {findAllByDisplayValue} from "@testing-library/react";
 import { MainContent } from './view/MainContent/MainContent';
+import { Footer } from './view/Footer/Footer';
 
 function App() {
   return (
       <div>
      <NavBar></NavBar>
      <MainContent></MainContent>
+     <Footer></Footer>
       </div>
   );
 }
---
+++ src/view/Footer/Footer.tsx
@@ -0,0 +1,12 @@
+import {Component} from "react";
+
+export class Footer extends Component {
+
+    render() {
+        return (
+            <div className="p-2 bg-blue-500">
+                <h1 className="text-1xl text-center text-secondaryColor">This is Footer</h1>
+            </div>
+        );
+    }
+}
