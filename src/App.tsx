f0471d386d0ca70bce06c7f4d8feb92d2da94850
SethminiDissanayake <sethminidissa@gmail.com>
2024-05-25 09:28:50 +0530

    create 3 functional components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,32 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {findAllByDisplayValue} from "@testing-library/react";
 
 function App() {
   return (
-    <h1> This is App.JS</h1>
+      <div>
+        <h1 className={"text-5xl text-red-500"}>This is App.js</h1>
+        <div style={{
+            backgroundColor:"red",
+            padding:10,
+            textAlign:"center"
+        }}> Inline Css Box</div>
+
+          <br/>
+
+          <div className="Box">
+              External Css Box
+          </div>
+
+          <br/>
+          <div className="bg-red-500 p-3 text-center">
+              External Css Box
+          </div>
+      </div>
+
+
+
   );
 }
 
