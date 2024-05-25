f8179be158db4576f835d688b68ca79e01656242
SethminiDissanayake <sethminidissa@gmail.com>
2024-05-25 14:45:08 +0530

    Linked Nav bar

--- src/view/common/Navbar/NavBar.tsx
+++ src/view/common/Navbar/NavBar.tsx
@@ -1,5 +1,6 @@
 import {Component} from "react";
 import icon from '../../../images/coca-leaves.png';
+import { Link } from "react-router-dom";
 
 export class NavBar extends Component {
 
@@ -10,9 +11,9 @@ export class NavBar extends Component {
             <img className="h-9 w-9 p-1" src={icon} alt="" />
 
             <ul className="list-none ml-[500px]">
-                <li className="inline-block text-[#0af712] mr-4 cursor-pointer hover:text-[#000000]">Home</li>
-                <li className="inline-block text-[#0af712] mr-4 cursor-pointer hover:text-[#000000]">About</li>
-                <li className="inline-block text-[#0af712] mr-4 cursor-pointer hover:text-[#000000]"> Contact</li>
+                <li className="inline-block text-[#0af712] mr-4 cursor-pointer hover:text-[#000000]"><Link to="/">Home</Link></li>
+                <li className="inline-block text-[#0af712] mr-4 cursor-pointer hover:text-[#000000]"> <Link to="/about">About</Link></li>
+                <li className="inline-block text-[#0af712] mr-4 cursor-pointer hover:text-[#000000]"><Link to="/contact">Contact</Link></li>
             </ul>
            </div>
         );
