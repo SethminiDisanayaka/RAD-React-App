db486defe7571c5d430759e1966d45088e37de8f
SethminiDissanayake <sethminidissa@gmail.com>
2024-05-25 11:00:31 +0530

    MainContent.tsx file created

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,13 @@ import logo from './logo.svg';
 // import './App.css';
 import {NavBar} from './view/Navbar/NavBar';
 import {findAllByDisplayValue} from "@testing-library/react";
+import { MainContent } from './view/MainContent/MainContent';
 
 function App() {
   return (
       <div>
      <NavBar></NavBar>
+     <MainContent></MainContent>
       </div>
   );
 }
---
+++ src/view/MainContent/MainContent.tsx
@@ -0,0 +1,12 @@
+import {Component} from "react";
+
+export class MainContent extends Component {
+
+    render() {
+        return (
+            <div className="md:px12 p-4 mx-auto bg-amber-100">
+                <h1 className="text-1xl text-center text-secondaryColor pt-28 pb-28">This is main content</h1>
+            </div>
+        );
+    }
+}
