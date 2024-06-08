318cb3414b9d676063541a44121181a4e4b72ffc
SethminiDissanayake <sethminidissa@gmail.com>
2024-06-08 13:27:55 +0530

    CartItem.ts file created

--- src/view/common/ModifyCart/ModifyCart.tsx
+++ src/view/common/ModifyCart/ModifyCart.tsx
@@ -10,6 +10,8 @@ interface ModifyCartState{
 
 export class ModifyCart extends Component <ModifyCartProps ,ModifyCartState>{
 
+    public static itemsList: CartItem[] =[]
+
     constructor (props :ModifyCartProps){
         super(props);
 
@@ -20,23 +22,27 @@ export class ModifyCart extends Component <ModifyCartProps ,ModifyCartState>{
 
     render() {
 
-    const {itemCount} =this.state;
+    let {itemCount} =this.state;
 
     const increaseItemCount =
     () => {
-     alert("Increase");
+    this.setState({
+          itemCount: ++itemCount
+          })
     }
 
     const decreaseItemCount =
         () => {
-        alert("Decrease");
+      this.setState({
+      itemCount: --itemCount
+      })
     }
 
 
             return (
                <div className=" w-full mt-1 p-[2.4px] text-[8px] text-center">
                                                <button className="float-left text-[8px] bg-yellow-300 rounded-lg h-3 w-4" onClick={decreaseItemCount}> - </button>
-                                               <small className="text-[8px]"> 1 </small>
+                                               <small className="text-[8px]"> {itemCount} </small>
                                                <button className="float-right text-[8px] bg-yellow-300 rounded-lg h-3 w-4" onClick={increaseItemCount}> + </button>
                                            </div>
             );
