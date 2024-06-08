be17071d1d686860c45374d63c0e31131c29dc92
SethminiDissanayake <sethminidissa@gmail.com>
2024-06-08 13:37:07 +0530

    ProductModel.ts file created

---
+++ src/Model/CartItem.ts
@@ -0,0 +1,4 @@
+export interface CartItem{
+    product : {},
+    itemCount : number
+}
--- src/view/common/ModifyCart/ModifyCart.tsx
+++ src/view/common/ModifyCart/ModifyCart.tsx
@@ -1,4 +1,5 @@
 import {Component} from "react";
+import { CartItem } from "../../../Model/CartItem";
 
 interface ModifyCartProps{
 
